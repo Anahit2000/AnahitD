ced736e14ca93dedb2068db2160641597efdf97a
Anahit2000 <an.dan00@mail.ru>
2020-01-30 16:03:48 +0400

    Anahiti arajin  gluxgorcoc@

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//
+//var kes=Math.floor(Math.sqrt(num))
+//
+//    var q=Number.isInteger(t)
+var num = 5
+var q= Math.floor(Math.sqrt(num))
+for (var i=2;i<=q;i++) {
+var t=num/i
+var isInt=true
+if(isInt){
+    document.write("baghadryal")
+        isInt=false;
+//    break;
+}
+    else{
+        document.write(hhhhhhhh)
+    }
+}
